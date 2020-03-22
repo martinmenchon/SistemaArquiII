ee40247778c97091537a398b250af9382fd854cf
Martín Menchón <martinmenchon@gmail.com>
2020-03-22 16:10:35 -0300

    Perfiles de paralelismo
    Signed-off-by: Martín Menchón <martinmenchon@gmail.com>

---
+++ Perfiles_de_paralelismo/precache-manifest.81621884c127f6eea6edd9f6e85572ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5f48fd71bd0554d3d29",
+    "url": "/SistemaArquiII/Perfiles_de_paralelismo/static/css/main.0ea70dc0.chunk.css"
+  },
+  {
+    "revision": "c5f48fd71bd0554d3d29",
+    "url": "/SistemaArquiII/Perfiles_de_paralelismo/static/js/main.3e837f5c.chunk.js"
+  },
+  {
+    "revision": "f739c91d17a33715a204",
+    "url": "/SistemaArquiII/Perfiles_de_paralelismo/static/js/runtime~main.e9246427.js"
+  },
+  {
+    "revision": "a219b7d1d0d55aecb7c2",
+    "url": "/SistemaArquiII/Perfiles_de_paralelismo/static/js/2.a2771d8b.chunk.js"
+  },
+  {
+    "revision": "812b8514c1b5a35401e1ce894a082b5f",
+    "url": "/SistemaArquiII/Perfiles_de_paralelismo/index.html"
+  }
+];
