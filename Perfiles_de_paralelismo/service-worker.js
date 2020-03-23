e34dd218f758cae15d390fd0b47023cf3d3166c0
Martín Menchón <martinmenchon@gmail.com>
2020-03-22 22:07:28 -0300

    Perfiles de paralelismo bug fixed
    Signed-off-by: Martín Menchón <martinmenchon@gmail.com>

--- Perfiles_de_paralelismo/service-worker.js
+++ Perfiles_de_paralelismo/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SistemaArquiII/Perfiles_de_paralelismo/precache-manifest.81621884c127f6eea6edd9f6e85572ac.js"
+  "/SistemaArquiII/Perfiles_de_paralelismo/precache-manifest.74450852efb26f154ddc3caac5f24007.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/SistemaArquiII/Perfiles_de_paralelismo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SistemaArquiII/Perfiles_de_paralelismo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
